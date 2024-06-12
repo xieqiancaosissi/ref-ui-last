2aac812cc0c6772f096012da5131e43d9ee8be9e
deep-path <476044723@qq.com>
2024-06-12 19:34:24 +0800

    feat: add device

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export const isMobile = (): Boolean => {
   return document.documentElement.clientWidth <= 1023;
@@ -22,8 +22,8 @@ export const useLargeScreen = () => {
   const handleResize = () => setLargeWindow(isLargeScreen());
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return largeWindow;
@@ -35,8 +35,8 @@ export const useMobile = () => {
   const handleResize = () => setMobileWindow(isMobile());
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return mobileWindow;
@@ -48,31 +48,31 @@ export const useClientMobile = () => {
   const handleResize = () => setMobileWindow(isClientMobie());
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return mobileWindow;
 };
 
 export enum ExplorerType {
-  Chrome = 'Chrome',
-  Firefox = 'Firefox',
-  Opera = 'Opera',
-  Safari = 'Safari',
+  Chrome = "Chrome",
+  Firefox = "Firefox",
+  Opera = "Opera",
+  Safari = "Safari",
 }
 
 export const getExplorer = () => {
   const explorer = window.navigator.userAgent;
-  if (explorer.indexOf('MSIE') >= 0) {
-    return 'ie';
-  } else if (explorer.indexOf('Firefox') >= 0) {
+  if (explorer.indexOf("MSIE") >= 0) {
+    return "ie";
+  } else if (explorer.indexOf("Firefox") >= 0) {
     return ExplorerType.Firefox;
-  } else if (explorer.indexOf('Chrome') >= 0) {
+  } else if (explorer.indexOf("Chrome") >= 0) {
     return ExplorerType.Chrome;
-  } else if (explorer.indexOf('Opera') >= 0) {
+  } else if (explorer.indexOf("Opera") >= 0) {
     return ExplorerType.Opera;
-  } else if (explorer.indexOf('Safari') >= 0) {
+  } else if (explorer.indexOf("Safari") >= 0) {
     return ExplorerType.Safari;
   }
 };

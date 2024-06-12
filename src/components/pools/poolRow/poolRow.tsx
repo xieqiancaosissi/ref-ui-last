f886d8184d53353a03eda136eb4a4a94b3306462
deep-path <476044723@qq.com>
2024-06-12 17:25:38 +0800

    feat: remove tokens file and add new style

--- src/components/pools/poolRow/poolRow.tsx
+++ src/components/pools/poolRow/poolRow.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "./poolRow.module.css";
 
 export default function PoolRow() {
-  return <div></div>;
+  return (
+    <>
+      <div className={styles.poolContainer}>
+        {/* tokens */}
+        <div></div>
+        <div>
+          {/* fee */}
+          <div>7.77%</div>
+          {/* apr */}
+          <div>
+            <span>0.36%</span>
+          </div>
+          {/* 24h */}
+          <div>$0.39</div>
+          {/* tvl */}
+          <div>$1.89k</div>
+        </div>
+      </div>
+    </>
+  );
 }

4f5ebf84550ea92052867b5b04e6a818af8cfeec
deep-path <476044723@qq.com>
2024-08-01 11:58:40 +0800

    feat: create pool

--- src/components/pools/createPoolModal/createPoolInput/index.tsx
+++ src/components/pools/createPoolModal/createPoolInput/index.tsx
@@ -3,6 +3,7 @@ import SelectTokenModal from "@/components/common/SelectTokenModal/Index";
 import { ArrowDownIcon } from "@/components/swap/icons";
 import { ITokenMetadata } from "@/interfaces/tokens";
 import HoverTooltip from "@/components/common/HoverToolTip";
+import { MobileArrowUp } from "../../icon";
 export default function PoolInput({
   title,
   index,
@@ -31,18 +32,21 @@ export default function PoolInput({
   }
   return (
     <>
-      <div className="flex flex-col items-start w-48 mb-2" onClick={showModal}>
+      <div
+        className="flex flex-col items-start lg:w-48 xsm:w-full mb-2"
+        onClick={showModal}
+      >
         <h2 className="text-gray-50 text-sm font-normal mb-3">{title}</h2>
         <div className="w-full h-16 rounded bg-dark-60 flex items-center justify-between border border-transparent px-4 hover:border-green-10">
-          <div className="w-35 frcc">
+          <div className="lg:w-35 xsm:w-full frcc">
             {selectToken?.icon ? (
               <img
                 src={selectToken?.icon}
                 alt=""
-                className="w-6 h-6 rounded-full"
+                className="lg:w-6 lg:h-6 xsm:w-9 xsm:h-9 rounded-full"
               />
             ) : (
-              <div className="w-6 h-6 rounded-full bg-dark-40 shrink-0"></div>
+              <div className="lg:w-6 lg:h-6 xsm:w-9 xsm:h-9 rounded-full bg-dark-40 shrink-0"></div>
             )}
             <div className="text-base font-medium text-white ml-2 w-full">
               {selectToken && selectToken?.symbol?.length > 11 ? (
@@ -54,7 +58,8 @@ export default function PoolInput({
               )}
             </div>
           </div>
-          <ArrowDownIcon className={"text-gray-50"} />
+          <ArrowDownIcon className={"text-gray-50 xsm:hidden"} />
+          <MobileArrowUp className={"lg:hidden"} />
         </div>
       </div>
       <SelectTokenModal
--- src/components/pools/icon.tsx
+++ src/components/pools/icon.tsx
@@ -6,6 +6,7 @@ export function MobileArrowUp(props: any) {
       viewBox="0 0 12 6"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path
         fillRule="evenodd"

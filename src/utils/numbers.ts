58c5aa64219003891bfab33ecaacb041bc83cc53
nature <nature@ref.finance>
2024-08-08 00:03:52 +0800

    add search ablity for dcl list

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -423,3 +423,13 @@ export const checkAllocations = (sum: string, allocations: string[]) => {
     ];
   } else return allocations;
 };
+export function generateRandomString(length: number) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+}

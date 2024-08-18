dfb3944cda7381f53c8b8c803cb442177f2d3c17
nature <nature@ref.finance>
2024-08-19 00:21:48 +0800

    add hover effectity

--- src/components/portfolio/components/WalletTokenList.tsx
+++ src/components/portfolio/components/WalletTokenList.tsx
@@ -79,54 +79,56 @@ export const WalletTokenList = ({
     }
   };
   return (
-    <div className="flex items-center w-full mb-6 text-white px-4">
-      <div className="flex items-center" style={{ width: "50%" }}>
-        <img className="w-6 h-6 rounded-3xl mr-2.5" src={token.icon} alt={""} />
-        <div className="text-sm">
-          <p className="w-24 overflow-hidden whitespace-nowrap overflow-ellipsis">
-            {token.symbol}
-          </p>
-          <p className="text-gray-50 text-xs">
-            {beautifyNumber({
-              num: getTokenPrice(token),
-              className: "text-gray-50 text-xs",
-              subClassName: "text-[8px]",
-              isUsd: true,
-            })}
-          </p>
+    <div className="w-full mb-3 px-2">
+      <div className="flex items-center  rounded-md hover:bg-gray-100 h-[46px] flex-grow px-2 text-white">
+        <div className="flex items-center" style={{ width: "50%" }}>
+          <img className="w-6 h-6 rounded-3xl mr-2.5" src={token.icon} alt={""} />
+          <div className="text-sm">
+            <p className="w-24 overflow-hidden whitespace-nowrap overflow-ellipsis">
+              {token.symbol}
+            </p>
+            <p className="text-gray-50 text-xs">
+              {beautifyNumber({
+                num: getTokenPrice(token),
+                className: "text-gray-50 text-xs",
+                subClassName: "text-[8px]",
+                isUsd: true,
+              })}
+            </p>
+          </div>
         </div>
+        <div className="flex flex-col text-sm" style={{ width: "25%" }}>
+          <span>
+            {display_number_internationalCurrencySystemLongString(
+              Big(tokenBalance || 0).toFixed()
+            )}
+          </span>
+          <span
+            className={`text-xs text-gray-50 ${showWithdraw ? "" : "hidden"}`}
+          >
+            {display_value(String(token?.t_value))}
+          </span>
+        </div>
+        {showWithdraw ? (
+          <div
+            className="flex items-center justify-center rounded border border-gray-300 text-xs text-gray-10 hover:text-white h-6 px-1 cursor-pointer"
+            onClick={doWithDraw}
+            style={{ width: "25%" }}
+          >
+            <ButtonTextWrapper
+              loading={withdrawLoading}
+              Text={() => <>Withdraw</>}
+            />
+          </div>
+        ) : (
+          <div
+            className="flex items-center justify-end text-sm"
+            style={{ width: "25%" }}
+          >
+            {display_value(String(token?.t_value))}
+          </div>
+        )}
       </div>
-      <div className="flex flex-col text-sm" style={{ width: "25%" }}>
-        <span>
-          {display_number_internationalCurrencySystemLongString(
-            Big(tokenBalance || 0).toFixed()
-          )}
-        </span>
-        <span
-          className={`text-xs text-gray-50 ${showWithdraw ? "" : "hidden"}`}
-        >
-          {display_value(String(token?.t_value))}
-        </span>
-      </div>
-      {showWithdraw ? (
-        <div
-          className="flex items-center justify-center rounded border border-gray-300 text-xs text-gray-10 hover:text-white h-6 px-1 cursor-pointer"
-          onClick={doWithDraw}
-          style={{ width: "25%" }}
-        >
-          <ButtonTextWrapper
-            loading={withdrawLoading}
-            Text={() => <>Withdraw</>}
-          />
-        </div>
-      ) : (
-        <div
-          className="flex items-center justify-end text-sm"
-          style={{ width: "25%" }}
-        >
-          {display_value(String(token?.t_value))}
-        </div>
-      )}
     </div>
   );
 };

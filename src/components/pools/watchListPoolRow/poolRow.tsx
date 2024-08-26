62a047dcb551b9acca79c47bf8581628a72b5d2f
deep-path <476044723@qq.com>
2024-08-26 18:18:06 +0800

    fix: mobile tknx

--- src/components/pools/watchListPoolRow/poolRow.tsx
+++ src/components/pools/watchListPoolRow/poolRow.tsx
@@ -169,6 +169,17 @@ export default function PoolRow({
                   }`}
                 >
                   {item?.token_account_ids?.map((ite: any, ind: number) => {
+                    if (item.pool_kind == "SIMPLE_POOL") {
+                      return (
+                        <TokenIconComponent
+                          key={ite.tokenId + ind}
+                          ite={ite}
+                          tokenIcons={tokenIcons}
+                          pureIdList={pureIdList}
+                          ind={ind}
+                        />
+                      );
+                    }
                     const len = item?.token_account_ids?.length;
                     // if tokenid in tokenIcons
                     return Reflect.has(tokenIcons, ite.tokenId) ? (

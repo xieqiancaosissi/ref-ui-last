7e875ef72f5ffd3822ce147207edba4b1baa4f50
nature <nature@ref.finance>
2024-09-06 17:58:11 +0800

    update-bitte-icon

--- src/components/menu/walletConfig.tsx
+++ src/components/menu/walletConfig.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
-import { LedgerIcon } from "./icons2";
-import { transform } from "lodash";
+import { LedgerIcon, BitteIcon } from "./icons2";
 
 export const walletIconConfig: Record<string, any> = {
   ledger: <LedgerIcon className="" style={{ transform: "scale(0.2)" }} />,
+  "bitte-wallet": <BitteIcon />,
 };

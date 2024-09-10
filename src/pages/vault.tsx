f8157eda20e6a2dd7cd64e6bdc5047694278f9ca
deep-path <476044723@qq.com>
2024-09-10 17:14:27 +0800

    feat:vault

--- src/pages/vault.tsx
+++ src/pages/vault.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ValutAssets from "@/components/vault/valutAssets";
 
 export default function vault() {
-  return <div>vault</div>;
+  return (
+    <div>
+      {/* menu */}
+      <ValutAssets />
+      <div className="h-[200px] w-full xsm:hidden"></div>
+      {/* list */}
+      <div className="lg:w-[1104px] xsm:w-full"></div>
+    </div>
+  );
 }

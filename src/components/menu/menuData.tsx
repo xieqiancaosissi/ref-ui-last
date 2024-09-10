50f4ba6064cfc4db3a1ea8f117471a2b1c8eb023
deep-path <476044723@qq.com>
2024-09-10 17:16:34 +0800

    feat:remove vault

--- src/components/menu/menuData.tsx
+++ src/components/menu/menuData.tsx
@@ -7,7 +7,6 @@ import {
   FarmsIcon,
   YoursIcon,
   StakeIcon,
-  VaultIcon,
 } from "./icons";
 import {
   Rainbow,
@@ -72,12 +71,6 @@ export function menuData(): IMenu[] {
           label: "Farms",
           icon: <FarmsIcon />,
           path: "/v2farms",
-        },
-        {
-          id: "vault",
-          label: "Vault",
-          icon: <VaultIcon />,
-          path: "/vault",
         },
         {
           id: "yours",
@@ -149,7 +142,6 @@ export const routeMapIds = {
   "earn-farms": ["/v2farms", "/v2farms/[id]"],
   "earn-stake": ["/xref"],
   "earn-yours": ["/yours"],
-  "earn-vault": ["/vault"],
   meme: ["/meme", "/memeAirdop"],
   "-": ["/risks"],
   "bridge-aggregate": ["/bridge", "/bridge/history"],
